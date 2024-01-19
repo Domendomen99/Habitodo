348e98cea6dda170fa5f924728cf645d427d4b36
326804 <326804@studenti.unimore.it>
2024-01-19 12:20:29 +0100

    modifiche minori

--- app/src/main/java/com/unimore/habitodo/vista/VistaDopoLogIn.java
+++ app/src/main/java/com/unimore/habitodo/vista/VistaDopoLogIn.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -13,11 +14,14 @@ import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.FirebaseDatabase;
 import com.unimore.habitodo.Applicazione;
 import com.unimore.habitodo.R;
 
 public class VistaDopoLogIn extends Fragment {
+
+    private TextView labelNomeUtente;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -30,7 +34,10 @@ public class VistaDopoLogIn extends Fragment {
     }
 
     private void inizializzaVista(View vista) {
-
+        this.labelNomeUtente = vista.findViewById(R.id.labelNomeUtente);
+        FirebaseAuth firebaseAuth = (FirebaseAuth) Applicazione.getInstance().getModello().getBean("firebaseAuth");
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        this.labelNomeUtente.setText(user.getEmail());
     }
 
     private void inizializzaAzioni() {
